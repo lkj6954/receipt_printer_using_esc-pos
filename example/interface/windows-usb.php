6b528d34ed8d853bace7be49b72306f615197292
lkj6954 <rltkwpdntm@naver.com>
2022-03-12 22:44:51 +0900

    complete korean print using iconv() and CP949

--- example/interface/windows-usb.php
+++ example/interface/windows-usb.php
@@ -5,6 +5,7 @@ require __DIR__ . '/../../vendor/autoload.php';
 use Mike42\Escpos\Printer;
 use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 
+
 /**
  * Install the printer using USB printing support, and the "Generic / Text Only" driver,
  * then share it (you can use a firewall so that it can only be seen locally).
@@ -25,23 +26,22 @@ try {
 
     /* Print a "Hello world" receipt" */
     $printer = new Printer($connector);
-    $printer->text("Hello SICPAMA!\n");
-    $printer->text("Hello SICPAMA!\n");
-    $printer->text("Hello SICPAMA!\n");
-    $printer->text("Hello SICPAMA!\n");
-    $printer->text("Hello SICPAMA!\n");
-    $printer->text("Hello SICPAMA!\n");
-    $printer->text("Hello SICPAMA!\n");
-    $printer->text("Hello SICPAMA!\n");
-    $printer->text("Hello SICPAMA!\n");
-    $printer->text("Hello SICPAMA!\n");
-    $printer->text("Hello SICPAMA!\n");
-    $printer->text("Hello SICPAMA!\n");
 
+    $printer->text(iconv("UTF-8", "CP949", "주문일시 : 22.03.12") . "\n");
+    $printer->text(iconv("UTF-8", "CP949", "5번 테이블") . "\n");
+    $printer->text(iconv("UTF-8", "CP949", "메뉴명 개수 유저닉네임") . "\n");
+    $printer->text(iconv("UTF-8", "CP949", "추천멘트") . "\n");
     $printer->cut();
 
     /* Close printer */
     $printer->close();
 } catch (Exception $e) {
     echo "Couldn't print to this printer: " . $e->getMessage() . "\n";
+}
+
+function title(Printer $printer, $str)
+{
+    $printer->selectPrintMode(Printer::MODE_DOUBLE_HEIGHT | Printer::MODE_DOUBLE_WIDTH);
+    $printer->text($str);
+    $printer->selectPrintMode();
 }
